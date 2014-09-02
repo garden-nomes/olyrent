70677c29dc1f6a00c925a31145b97992f5639761
noahhjw <noahbweiner@gmail.com>
2014-09-02 10:01:26 -0700

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Olyrent::Application.config.secret_key_base = 'ab2c25c8cfa55b359aff834339b452f29289921bd1d48bcc6faef4ef659f951b68b7d96accbc9ec6d67ac7ec99253a213607f3fc69c14da5e5ed54501936cd2b'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Olyrent::Application.config.session_store :cookie_store, key: '_olyrent_session'
